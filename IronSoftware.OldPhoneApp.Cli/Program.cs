150e1e8682c9b5d00ace9d8f89499bae6c0a2bb4
Dario Bravo <dbravo@basecodeit.com>
2025-07-17 14:55:53 -0300

    Initial commit

---
+++ IronSoftware.OldPhoneApp.Cli/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace IronSoftware.OldPhoneApp.Cli;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
